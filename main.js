78ae8c60c86d5bd624d233bd6c7a79e23a9beb33
Mathew Glass <hello@mathew.glass>
2022-03-03 16:28:12 -0500

    Added Lists containing colors and sizes

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                { id: 1, size: 'x-small' },
+                { id: 2, size: 'small' },
+                { id: 3, size: 'medium' },
+                { id: 4, size: 'large' },
+                { id: 5, size: 'x-large' }
+            ]
+            
         }
     }
 })
